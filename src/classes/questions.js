5e1ee2180972e0fd02cf3ca9c5b6efeaf6c7da97
Jordan Hitch <thinkin.arbys@gmail.com>
2017-02-28 17:06:38 -0800

    fixed typo in questions

--- src/classes/questions.js
+++ src/classes/questions.js
@@ -1,7 +1,7 @@
 const Questions = [
     { plural: 'Which values are most reflected in your life right now?', singular: 'Which value is more reflected in your life right now?' },
     { plural: 'Which values would you like your life to reflect in the future?', singular: 'Which value would you more like your life to reflect in the future?' },
-    { plural: 'Which values, when violated, make you most angry?', singular: 'Which value, when violated, makes you most angrier?' },
+    { plural: 'Which values, when violated, make you most angry?', singular: 'Which value, when violated, makes you angrier?' },
     { plural: 'Which values do you appreciate most when enacted by other people?', singular: 'Which value do you appreciate more when enacted by other people?' },
     { plural: 'Which values are most important for humanity or society to succeed?', singular: 'Which value is more important in order for humanity or society to succeed?' },
     { plural: 'Which values should be most represented and upheld by our laws?', singular: 'Which value is it more important to be represented and upheld by our laws?' }
